057b098ea3cb9d46b05df813756c3a202d7ac3a9
woodbine <ian@spendnetwork.com>
2015-04-09 20:14:35 +0100

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -30,7 +30,7 @@ for block in blocks:
 
 	link = block.li.a['href']
 	titles = block.findAll('div',{'class':'inner2'})
-	titles = title.contents[0]
+	titles = titles.contents[0]
 	
 	for title in titles:
 		print title
